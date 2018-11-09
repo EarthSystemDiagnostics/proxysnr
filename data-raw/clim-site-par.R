c73e1f009e173044f69d1c80ca0465b764ffd8d7
Thomas Münch <tmuench@awi.de>
2018-11-09 17:40:34 +0100

    Added diffusion length data and calculateion source code.

---
+++ data-raw/clim-site-par.R
@@ -0,0 +1,16 @@
+## clim-site-par.R: Read in site parameters to provide in package
+##
+## Author: Thomas Münch (thomas.muench@awi.de), Alfred-Wegener-Institut, 2018
+##
+
+library(devtools)
+
+dml <- read.table("data-raw/dml_diffusion-par.txt",
+                  header = TRUE, skip = 10, sep = "\t")
+wais <- read.table("data-raw/wais_diffusion-par.txt",
+                   header = TRUE, skip = 14, sep = "\t")
+
+clim.site.par <- rbind(dml, wais)
+
+devtools::use_data(clim.site.par, overwrite = TRUE)
+
